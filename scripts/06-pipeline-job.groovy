52a6283a7f5d6bb60c1e92ae62e5e3494628e924
Bideep Bezbaruah <bbideep@googlemail.com>
2019-01-15 07:01:47 +0000

    06-pipeline-job.groovy created online with Bitbucket

---
+++ scripts/06-pipeline-job.groovy
@@ -0,0 +1,40 @@
+import hudson.plugins.git.*;
+import jenkins.model.Jenkins;
+
+def user = new File("/run/secrets/scm-repo-user").text.trim()
+
+def getCredentialsId = { username ->
+    def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
+        jenkins.model.Jenkins.instance
+    )
+
+    def c = creds.findResult { it.username == username ? it : null }
+
+    if ( c ) {
+
+        def systemCredentialsProvider = jenkins.model.Jenkins.instance.getExtensionList(
+            'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+            ).first()
+
+        def id = systemCredentialsProvider.credentials.first().id
+        return id
+    } else {
+      println "could not find credential for ${username}"
+    }
+}
+
+def GIT_REPO_HTTPS_URL = System.env.GIT_REPO_HTTPS_URL
+def GIT_BRANCH = System.env.GIT_BRANCH
+//def scm = new GitSCM("https://git-codecommit.us-east-1.amazonaws.com/v1/repos/jenkins-pipeline")
+def scm = new GitSCM(GIT_REPO_HTTPS_URL)
+scm.branches = [new BranchSpec("*/"+GIT_BRANCH)];
+scm.userRemoteConfigs = [new UserRemoteConfig(GIT_REPO_HTTPS_URL, null, null, getCredentialsId(user))]
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
+
+def parent = Jenkins.instance
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Pipeline_Demo")
+job.definition = flowDefinition
+
+parent.reload()
